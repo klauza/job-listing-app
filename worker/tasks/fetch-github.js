7a9471af9ebaac81a1bf0625324f027a33f75f79
Michal Klauza <klauza.uk@gmail.com>
2020-05-14 14:18:05 +0100

    setting up redis

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,5 +1,12 @@
 var fetch = require('node-fetch');
 
+const redis = require("redis");
+const client = redis.createClient();
+
+const { promisify } = require("util");
+const getAsync = promisify(client.get).bind(client);  // it ocnverts client.get function to the promise
+const setAsync = promisify(client.get).bind(client);
+
 const baseUrl = `https://jobs.github.com/positions.json`;
 
 async function fetchGithub(){

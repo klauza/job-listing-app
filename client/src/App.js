19fc35be573398014f5b1d238d29e910b8fef7e5
Michal Klauza <klauza.uk@gmail.com>
2020-05-14 15:41:26 +0100

    fetch func

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 import Jobs from './Jobs';
 
-function App() {
+const JOB_API_URL = "http://localhost:3001/jobs";
 
-  // API
-  const mockJobs = [
-    {
-      title: 'SWE 1', company: 'Google'
-    },
-    {
-      title: 'SWE 2', company: 'Apple'
-    },
-    {
-      title: 'SWE 3', company: 'Facebook'
-    }
-  ]
+// API
+const mockJobs = [
+  {
+    title: 'SWE 1', company: 'Google'
+  },
+  {
+    title: 'SWE 2', company: 'Apple'
+  },
+  {
+    title: 'SWE 3', company: 'Facebook'
+  }
+]
+
+// fetch jobs from api
+async function fetchJobs(){
+  // browser version of fetch, its built-into the browser
+  const res = await fetch(JOB_API_URL);
+  const json = await res.json();
+
+  console.log({json});
+}
+
+
+function App() {
+  const [jobList, updateJobs] = React.useState([]);
+
+
+  // useeffect takes anonymous function in which we can do:
+  // some function calls
+  // laso has second argument which is for hooks we want to watch for update, if its empty, fires once (like componentdidmount)
+  React.useEffect(()=>{
+    fetchJobs();
+  }, [])
+  
   return (
     <div className="App">
-      <Jobs jobs={mockJobs} />
+      <Jobs jobs={jobList} />
     </div>
   );
 }

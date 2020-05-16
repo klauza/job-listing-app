a7af531a3d68d554745762f4ed881442aafe80d9
Michal Klauza <klauza.uk@gmail.com>
2020-05-16 11:31:54 +0100

    uncheck build from .gitignore

---
+++ client/build/precache-manifest.96dab8412c33130aae069f51f489684d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53dce74369e31598c2c979ad4c633d37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0d59331c1d9fc8f5169",
+    "url": "/static/css/main.c3d96f79.chunk.css"
+  },
+  {
+    "revision": "bbe6710eb63c7819e9ea",
+    "url": "/static/js/2.4d84091c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4d84091c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d59331c1d9fc8f5169",
+    "url": "/static/js/main.9f5c7281.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
